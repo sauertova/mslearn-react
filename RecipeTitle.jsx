f303d55c97cb8fab3d333586c2fc39e2ebe8b7bd
sauertova <148710594+sauertova@users.noreply.github.com>
2024-01-07 12:08:28 -0500

    Add files via upload

---
+++ RecipeTitle.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// TODO: Create RecipeTitle component
+function RecipeTitle(props) {
+    return (
+        <section>
+            <h2>{ props.title }</h2>
+        </section>
+    )
+};
+
+export default RecipeTitle;
